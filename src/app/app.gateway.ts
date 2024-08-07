502929103c5919e887f23af2a266295c00b7fc16
Soni <soni@recalstudios.net>
2024-08-08 01:32:27 +0200

    feat: create lobby codes

--- src/app/app.gateway.ts
+++ src/app/app.gateway.ts
@@ -2,8 +2,44 @@ import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 
 @WebSocketGateway()
 export class AppGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  /**
+   * A list of the currently active game lobbies
+   *
+   * @private
+   */
+  private lobbies: string[] = [];
+
+  /**
+   * Creates a game lobby and returns the join code
+   */
+  @SubscribeMessage('create')
+  createLobby(): string {
+    const code = this.createLobbyCode(4);
+    this.lobbies.push(code);
+    return code;
+  }
+
+  /**
+   * Creates a unique lobby code
+   *
+   * @param length How many symbols the code will include
+   * @returns A unique lobby code
+   * @private
+   */
+  private createLobbyCode(length: number): string {
+    const validSymbols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let output: string;
+
+    // Repeatedly generate new codes until the output is unique
+    do {
+      output = '';
+      for (let i = 0; i < length; i++) {
+        output += validSymbols.charAt(
+          Math.floor(Math.random() * validSymbols.length),
+        );
+      }
+    } while (this.lobbies.includes(output));
+
+    return output;
   }
 }
