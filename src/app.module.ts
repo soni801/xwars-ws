3a1b0fec31bead32a1020c6cdb10ce103c121379
Soni <soni@recalstudios.net>
2024-08-08 01:04:06 +0200

    chore: remove unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AppGateway } from './app/app.gateway';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService, AppGateway],
+  providers: [AppGateway],
 })
 export class AppModule {}
