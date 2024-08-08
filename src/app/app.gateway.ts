c9ff7804892d170a2ca276d0fd346c698ee241e5
Soni <soni@recalstudios.net>
2024-08-08 02:12:03 +0200

    feat: let other sockets join a created lobby

--- src/app/app.gateway.ts
+++ src/app/app.gateway.ts
@@ -1,4 +1,5 @@
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway()
 export class AppGateway {
@@ -11,12 +12,38 @@ export class AppGateway {
 
   /**
    * Creates a game lobby and returns the join code
+   *
+   * @param socket The Socket making the request
+   * @returns The join code of the created lobby
    */
   @SubscribeMessage('create')
-  createLobby(): string {
+  createLobby(socket: Socket): string {
+    // Create the lobby with a new code
     const code = this.createLobbyCode(4);
     this.lobbies.push(code);
+
+    // Add this socket to a socket.io room with the lobby code
+    socket.join(code);
+
     return code;
+  }
+
+  /**
+   * Joins a game lobby with the given code
+   *
+   * @param socket The Socket making the request
+   * @param code The lobby code to join
+   * @returns Whether the join attempt was successful
+   */
+  @SubscribeMessage('join')
+  joinLobby(socket: Socket, code: string): boolean {
+    // Make sure that the lobby exists
+    if (!this.lobbies.includes(code)) return false;
+
+    // Add this socket to the socket.io room
+    socket.join(code);
+
+    return true;
   }
 
   /**
